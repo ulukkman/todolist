5324812475bfa9a80c3135310ce65c84d4d4f125
Ulukkman <ulukkman@users.noreply.github.com>
2021-01-21 17:00:21 +0600

    adding styles and logic to buttons

--- main/views.py
+++ main/views.py
@@ -44,3 +44,10 @@ def delete_todo(request, id):
     todo.delete()
     return redirect(test)
 
+def mark_todo(request, id):
+    todo = ToDo.objects.get(id=id)
+    todo.is_favorite = True
+    todo.save()
+    return redirect(test)
+
+
--- todo/urls.py
+++ todo/urls.py
@@ -29,5 +29,6 @@ urlpatterns = [
     path("add-todo/", add_todo, name="add-todo"),
     path("add-books/", add_books, name="add-books"),
     path("delete-todo/<id>/", delete_todo, name="delete-todo"),
+    path("mark-todo/<id>/", mark_todo, name="mark-todo"),
 ]   + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) \
     + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
