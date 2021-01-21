255deb6044f9a017beecaab0d89647265c0b52c7
Ulukkman <ulukkman@users.noreply.github.com>
2021-01-21 06:41:13 +0600

    connected style in todolist

--- todo/main/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import ToDo
-
-
-admin.site.register(ToDo)
--- todo/main/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-
-class ToDo(models.Model):
-    text = models.CharField(max_length=100)
-    created_at = models.DateField(auto_now_add=True)
-    is_closed = models.BooleanField(default=False)
-    is_favorite = models.BooleanField(default=False)
-
